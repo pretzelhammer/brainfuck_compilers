34d50f59ef8e8b9ff0a448d56d6d99a3253c31ed
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2020-10-18 08:54:27 -0400

    finishes parser and interpreter update

---
+++ src/bf_interpreter.rs
@@ -0,0 +1,269 @@
+use brainfuck_compilers::{ parse, ARRAY_LEN, EOF, Error, Inst};
+
+use std::env;
+use std::io;
+use std::io::{Read, Write, Bytes};
+
+fn run<R: Read, W: Write>(instructions: &[Inst], mut input: Bytes<R>, output: &mut W) -> Result<(), Error> {
+    let mut memory = [0_u8; ARRAY_LEN];
+    let mut ptr = 0;
+    let mut inst_ptr = 0;
+
+    while inst_ptr < instructions.len() {
+        match instructions[inst_ptr] {
+            Inst::IncPtr(n) => {
+                if ptr + n >= ARRAY_LEN {
+                    return Err(Error::PtrAboveLimit);
+                }
+                ptr += n;
+                inst_ptr += 1;
+            },
+            Inst::DecPtr(n) => {
+                if n > ptr {
+                    return Err(Error::PtrBelowZero);
+                }
+                ptr -= n;
+                inst_ptr += 1;
+            },
+            Inst::IncByte(n) => {
+                memory[ptr] = memory[ptr].wrapping_add(n as u8);
+                inst_ptr += 1;
+            },
+            Inst::DecByte(n) => {
+                memory[ptr] = memory[ptr].wrapping_sub(n as u8);
+                inst_ptr += 1;
+            },
+            Inst::WriteByte(n) => {
+                for _ in 0..n {
+                    let result = output.write(&[memory[ptr]]);
+                    match result {
+                        Ok(bytes_written) if bytes_written < 1 => {
+                            panic!("failed to write byte {} to output", memory[ptr]);
+                        },
+                        Err(error) => {
+                            panic!("failed to write byte to put with error {}", error);
+                        },
+                        Ok(_) => (), // everything went fine
+                    }
+                }
+                inst_ptr += 1;
+            },
+            Inst::ReadByte(n) => {
+                for _ in 0..n {
+                    let maybe_byte = input.next();
+                    match maybe_byte {
+                        Some(Ok(byte)) => {
+                            memory[ptr] = byte;
+                        },
+                        None => { // received EOF
+                            memory[ptr] = EOF;
+                        },
+                        Some(Err(error)) => {
+                            panic!("error while trying to read byte from input {}", error);
+                        },
+                    }
+                }
+                inst_ptr += 1;
+            },
+            Inst::LoopStart(_, goto) => {
+                if memory[ptr] == 0 {
+                    inst_ptr = goto;
+                } else {
+                    inst_ptr += 1;
+                }
+            },
+            Inst::LoopEnd(_, goto) => {
+                if memory[ptr] != 0 {
+                    inst_ptr = goto;
+                } else {
+                    inst_ptr += 1;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn parse_and_run<R: Read, W: Write>(src: &str, input: Bytes<R>, output: &mut W) -> Result<(), Error> {
+    let instructions = parse(src)?;
+    run(&instructions, input, output)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("USEAGE: program path/to/brainfuck/src.b");
+        std::process::exit(1);
+    }
+
+    let src = std::fs::read_to_string(&args[1])?;
+    let stdin = io::stdin();
+    let input = stdin.lock().bytes();
+    let stdout = io::stdout();
+    let mut output = stdout.lock();
+
+    parse_and_run(&src, input, &mut output)?;
+    output.flush()?;
+    
+    Ok(())
+}
+
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    fn run_hello_world() {
+        let src = include_str!("../input/hello_world.b");
+        let input = [].bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            "Hello World!\n",
+            output_str,
+        );
+    }
+
+    #[test]
+    fn run_mini_hello_world() {
+        let src = include_str!("../input/mini_hello_world.b");
+        let input = [].bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            "hello world",
+            output_str,
+        );
+    }
+
+    #[test]
+    fn run_fibonacci() {
+        let src = include_str!("../input/fibonacci.b");
+        let input = [].bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            "1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89",
+            output_str,
+        );
+    }
+
+    #[test]
+    fn run_prints_abc() {
+        let src = include_str!("../input/prints_abc.b");
+        let input = [].bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            "abc\n",
+            output_str,
+        );
+    }
+
+    #[test]
+    fn run_quine() {
+        let src = include_str!("../input/quine.b");
+        let input = [].bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            src,
+            output_str,
+        );
+    }
+
+    #[test]
+    fn run_head() {
+        let src = include_str!("../input/head.b");
+        let input_str = "1a\n2b\n3c\n4d\n5e\n6f\n7g\n8h\n9i\n10j\n";
+        let input = input_str.as_bytes().bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            input_str,
+            output_str,
+        );
+    }
+
+    #[test]
+    fn run_squares() {
+        let src = include_str!("../input/squares.b");
+        let input = [].bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            "0\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100\n121\n144\n169\n196\n225\n256\n289\n324\n361\n400\n441\n484\n529\n576\n625\n676\n729\n784\n841\n900\n961\n1024\n1089\n1156\n1225\n1296\n1369\n1444\n1521\n1600\n1681\n1764\n1849\n1936\n2025\n2116\n2209\n2304\n2401\n2500\n2601\n2704\n2809\n2916\n3025\n3136\n3249\n3364\n3481\n3600\n3721\n3844\n3969\n4096\n4225\n4356\n4489\n4624\n4761\n4900\n5041\n5184\n5329\n5476\n5625\n5776\n5929\n6084\n6241\n6400\n6561\n6724\n6889\n7056\n7225\n7396\n7569\n7744\n7921\n8100\n8281\n8464\n8649\n8836\n9025\n9216\n9409\n9604\n9801\n10000\n",
+            output_str,
+        );
+    }
+
+    #[test]
+    fn run_rot13() {
+        let src = include_str!("../input/rot13.b");
+        let input_str = "unencrypted string";
+        let input = input_str.as_bytes().bytes();
+        let mut output = Vec::new();
+
+        let result = parse_and_run(src, input, &mut output);
+        assert!(result.is_ok());
+
+        let output_result = std::str::from_utf8(&output);
+        assert!(output_result.is_ok());
+        let output_str = output_result.unwrap();
+        
+        assert_eq!(
+            "harapelcgrq fgevat",
+            output_str,
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ const INSTRUCTIONS: &[char; 8] = &[
 ];
 
 pub const ARRAY_LEN: usize = 30_000;
-pub const NEWLINE: u8 = 0;
+pub const NEWLINE: u8 = 10;
 pub const EOF: u8 = 0;
 
 #[derive(Debug, Eq, PartialEq)]
@@ -36,16 +36,58 @@ pub enum Inst {
     LoopEnd(usize, usize),
 }
 
-fn map_char_to_inst(c: char, loop_depth: usize) -> Inst {
+impl Inst {
+    fn increment(&mut self) {
+        use Inst::*;
+        match self {
+            IncPtr(n) => *n += 1,
+            DecPtr(n) => *n += 1,
+            IncByte(n) => *n += 1,
+            DecByte(n) => *n += 1,
+            WriteByte(n) => *n += 1,
+            ReadByte(n) => *n += 1,
+            LoopStart(n, _) => *n += 1,
+            LoopEnd(n, _) => *n += 1,
+        }
+    }
+
+    fn same_as(&self, inst: &Inst) -> bool {
+        use Inst::*;
+        match (self, inst) {
+            (IncPtr(_), IncPtr(_)) => true,
+            (DecPtr(_), DecPtr(_)) => true,
+            (IncByte(_), IncByte(_)) => true,
+            (DecByte(_), DecByte(_)) => true,
+            (WriteByte(_), WriteByte(_)) => true,
+            (ReadByte(_), ReadByte(_)) => true,
+            (LoopStart(_, _), LoopStart(_, _)) => true,
+            (LoopEnd(_, _), LoopEnd(_, _)) => true,
+            _ => false,
+        }
+    }
+
+    fn update_goto(&mut self, updated_goto: usize) {
+        use Inst::*;
+        match self {
+            LoopStart(_, prev_goto) => *prev_goto = updated_goto,
+            LoopEnd(_, prev_goto) => *prev_goto = updated_goto,
+            _ => panic!("tried to update goto on instruction {:?}", self),
+        }
+    }
+}
+
+fn char_to_inst(c: char) -> Inst {
+    use Inst::*;
+
     match c {
-        INC_PTR => Inst::IncPtr,
-        DEC_PTR => Inst::DecPtr,
-        INC_BYTE => Inst::IncByte,
-        DEC_BYTE => Inst::DecByte,
-        WRITE_BYTE => Inst::WriteByte,
-        READ_BYTE => Inst::ReadByte,
-        LOOP_START => Inst::LoopStart(loop_depth),
-        LOOP_END => Inst::LoopEnd(loop_depth),
+        INC_PTR => IncPtr(1),
+        DEC_PTR => DecPtr(1),
+        INC_BYTE => IncByte(1),
+        DEC_BYTE => DecByte(1),
+        WRITE_BYTE => WriteByte(1),
+        READ_BYTE => ReadByte(1),
+        LOOP_START => LoopStart(1, 0),
+        LOOP_END => LoopEnd(1, 0),
         _ => panic!("unknown bf instruction {}", c),
     }
 }
@@ -55,97 +97,348 @@ pub enum Error {
     PtrBelowZero,
     PtrAboveLimit,
     UnbalancedBrackets,
+    InfiniteLoop,
 }
 
 impl std::error::Error for Error {}
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Error::*;
         match self {
-            Error::PtrBelowZero => write!(f, "error: mem pointer went below zero"),
-            Error::PtrAboveLimit => write!(f, "error: mem pointer went above limit {}", MEM_LIMIT),
-            Error::UnbalancedBrackets => write!(f, "error: unbalanced brackets in bf source"),
+            PtrBelowZero => write!(f, "error: mem pointer went below zero"),
+            PtrAboveLimit => write!(f, "error: mem pointer went above limit {}", ARRAY_LEN),
+            UnbalancedBrackets => write!(f, "error: unbalanced brackets in bf source"),
+            InfiniteLoop => write!(f, "error: pointless potential infinite loop in bf source"),
         }
     }
 }
 
 pub fn parse(src: &str) -> Result<Vec<Inst>, Error> {
     let mut loop_depth: usize = 0;
-    let mut instructions = Vec::new();
-    for c in src.chars() {
-        if INSTRUCTIONS.contains(&c) {
-            if c == LOOP_END {
-                if loop_depth == 0 {
-                    return Err(Error::UnbalancedBrackets);
-                }
-                loop_depth -= 1;
+    let mut instructions: Vec<Inst> = Vec::new();
+
+    // first pass, convert chars to run-length-encoded instructions
+    for c in src.chars().filter(|c| INSTRUCTIONS.contains(c)) {
+        if c == LOOP_END {
+            if loop_depth == 0 {
+                return Err(Error::UnbalancedBrackets);
             }
-            instructions.push(
-                map_char_to_inst(c, loop_depth)
-            );
-            if c == LOOP_START {
-                loop_depth += 1;
+            loop_depth -= 1;
+        } else if c == LOOP_START {
+            loop_depth += 1;
+        }
+
+        let curr_inst = char_to_inst(c);
+        if let Some(prev_inst) = instructions.last_mut() {
+            if prev_inst.same_as(&curr_inst) {
+                prev_inst.increment();
+            } else {
+                instructions.push(curr_inst);
             }
+        } else {
+            instructions.push(curr_inst);
         }
     }
+
     if loop_depth > 0 {
         return Err(Error::UnbalancedBrackets);
     }
+
+    // 2nd pass, link loops by setting their gotos
+    for i in 0..instructions.len() {
+        let mut update_goto: Option<usize> = None;
+        let curr_inst = &instructions[i];
+        match curr_inst {
+            Inst::LoopStart(n, _) => {
+                let mut mut_n = *n;
+                for j in i+1..instructions.len() {
+                    let next_inst = &instructions[j];
+                    match next_inst {
+                        Inst::LoopEnd(m, _) => {
+                            if *m >= mut_n {
+                                update_goto = Some(j+1);
+                                break;
+                            }
+                            mut_n -= m;
+                        },
+                        Inst::LoopStart(m, _) => {
+                            mut_n += m;
+                        },
+                        _ => (),
+                    }
+                }
+            },
+            Inst::LoopEnd(n, _) => {
+                let mut mut_n = 0; //*n;
+                for j in (0..i).rev() {
+                    let prev_inst = &instructions[j];
+                    match prev_inst {
+                        Inst::LoopStart(m, _) => {
+                            if *m > mut_n {
+                                update_goto = Some(j+1);
+                                if j+1 == i {
+                                    return Err(Error::InfiniteLoop);
+                                }
+                                break;
+                            }
+                            mut_n -= m;
+                        },
+                        Inst::LoopEnd(m, _) => {
+                            mut_n += m;
+                        },
+                        _ => (),
+                    }
+                }
+            },
+            _ => (),
+        }
+        if let Some(goto) = update_goto {
+            instructions[i].update_goto(goto);
+        }
+    }
+
     Ok(instructions)
 }
 
 mod tests {
     #[allow(unused_imports)]
     use super::*;
+    #[allow(unused_imports)]
+    use super::Inst::*;
 
     #[test]
-    fn simple_parse() {
-        let instructions = parse("><+-,.[]").unwrap();
+    fn parse_empty() {
+        let instructions = parse("").unwrap();
+        assert_eq!(&instructions, &[]);
+    }
+
+    #[test]
+    fn parse_inc_ptr() {
+        let instructions = parse(">").unwrap();
+        assert_eq!(&instructions, &[IncPtr(1)]);
+    }
+
+    #[test]
+    fn parse_inc_ptrs() {
+        let instructions = parse(">>>").unwrap();
+        assert_eq!(&instructions, &[IncPtr(3)]);
+    }
+
+    #[test]
+    fn parse_dec_ptr() {
+        let instructions = parse("<").unwrap();
+        assert_eq!(&instructions, &[DecPtr(1)]);
+    }
+
+    #[test]
+    fn parse_dec_ptrs() {
+        let instructions = parse("<<<").unwrap();
+        assert_eq!(&instructions, &[DecPtr(3)]);
+    }
+
+    #[test]
+    fn parse_inc_byte() {
+        let instructions = parse("+").unwrap();
+        assert_eq!(&instructions, &[IncByte(1)]);
+    }
+
+    #[test]
+    fn parse_inc_bytes() {
+        let instructions = parse("+++").unwrap();
+        assert_eq!(&instructions, &[IncByte(3)]);
+    }
+
+    #[test]
+    fn parse_dec_byte() {
+        let instructions = parse("-").unwrap();
+        assert_eq!(&instructions, &[DecByte(1)]);
+    }
+
+    #[test]
+    fn parse_dec_bytes() {
+        let instructions = parse("---").unwrap();
+        assert_eq!(&instructions, &[DecByte(3)]);
+    }
+
+    #[test]
+    fn parse_read_byte() {
+        let instructions = parse(",").unwrap();
+        assert_eq!(&instructions, &[ReadByte(1)]);
+    }
+
+    #[test]
+    fn parse_read_bytes() {
+        let instructions = parse(",,,").unwrap();
+        assert_eq!(&instructions, &[ReadByte(3)]);
+    }
+
+    #[test]
+    fn parse_write_byte() {
+        let instructions = parse(".").unwrap();
+        assert_eq!(&instructions, &[WriteByte(1)]);
+    }
+
+    #[test]
+    fn parse_write_bytes() {
+        let instructions = parse("...").unwrap();
+        assert_eq!(&instructions, &[WriteByte(3)]);
+    }
+
+    #[test]
+    fn parse_loop() {
+        let instructions = parse("[-]").unwrap();
         assert_eq!(
             &instructions,
             &[
-                Inst::IncPtr,
-                Inst::DecPtr,
-                Inst::IncByte,
-                Inst::DecByte,
-                Inst::ReadByte,
-                Inst::WriteByte,
-                Inst::LoopStart(0),
-                Inst::LoopEnd(0),
-            ]
+                LoopStart(1, 3),
+                DecByte(1),
+                LoopEnd(1, 1),
+            ],
         );
     }
 
     #[test]
-    fn parse_matching_loops() {
-        let instructions = parse("[][[]][[[]]]").unwrap();
+    fn parse_loops() {
+        let instructions = parse("[[[-]]]").unwrap();
         assert_eq!(
             &instructions,
             &[
-                Inst::LoopStart(0),
-                Inst::LoopEnd(0),
-                Inst::LoopStart(0),
-                Inst::LoopStart(1),
-                Inst::LoopEnd(1),
-                Inst::LoopEnd(0),
-                Inst::LoopStart(0),
-                Inst::LoopStart(1),
-                Inst::LoopStart(2),
-                Inst::LoopEnd(2),
-                Inst::LoopEnd(1),
-                Inst::LoopEnd(0),
-            ]
+                LoopStart(3, 3),
+                DecByte(1),
+                LoopEnd(3, 1),
+            ],
         );
     }
 
     #[test]
-    fn parse_missing_open_loop() {
-        let result = parse("[]]");
+    fn parse_all() {
+        let instructions = parse("[><+-,.]").unwrap();
+        assert_eq!(
+            &instructions,
+            &[
+                LoopStart(1, 8),
+                IncPtr(1),
+                DecPtr(1),
+                IncByte(1),
+                DecByte(1),
+                ReadByte(1),
+                WriteByte(1),
+                LoopEnd(1, 1),
+            ],
+        );
+    }
+
+    #[test]
+    fn parse_alls() {
+        let instructions = parse("[[[>>><<<+++---,,,...]]]").unwrap();
+        assert_eq!(
+            &instructions,
+            &[
+                LoopStart(3, 8),
+                IncPtr(3),
+                DecPtr(3),
+                IncByte(3),
+                DecByte(3),
+                ReadByte(3),
+                WriteByte(3),
+                LoopEnd(3, 1),
+            ],
+        );
+    }
+
+    #[test]
+    fn parse_uneven_loop_starts() {
+        let instructions = parse("[>[>[-]]]").unwrap();
+        assert_eq!(
+            &instructions,
+            &[
+                LoopStart(1, 7),
+                IncPtr(1),
+                LoopStart(1, 7),
+                IncPtr(1),
+                LoopStart(1, 7),
+                DecByte(1),
+                LoopEnd(3, 5),
+            ],
+        );
+    }
+
+    #[test]
+    fn parse_uneven_loop_ends() {
+        let instructions = parse("[[[-]>]>]").unwrap();
+        assert_eq!(
+            &instructions,
+            &[
+                LoopStart(3, 7),
+                DecByte(1),
+                LoopEnd(1, 1),
+                IncPtr(1),
+                LoopEnd(1, 1),
+                IncPtr(1),
+                LoopEnd(1, 1),
+            ],
+        );
+    }
+
+    #[test]
+    fn parse_uneven_loops_1() {
+        let instructions = parse("[[[>[[-]>]>]]>]").unwrap();
+        assert_eq!(
+            &instructions,
+            &[
+                LoopStart(3, 11),   // 0
+                IncPtr(1),
+                LoopStart(2, 7),    // 2
+                DecByte(1),
+                LoopEnd(1, 3),      // 4
+                IncPtr(1),
+                LoopEnd(1, 3),      // 6
+                IncPtr(1),
+                LoopEnd(2, 1),      // 8
+                IncPtr(1),
+                LoopEnd(1, 1),      // 10
+            ],
+        );
+    }
+
+    #[test]
+    fn parse_uneven_loops_2() {
+        let instructions = parse("[>[[>[>[[-]>]]>]]]").unwrap();
+        assert_eq!(
+            &instructions,
+            &[
+                LoopStart(1, 13),    // 0
+                IncPtr(1),
+                LoopStart(2, 13),    // 2
+                IncPtr(1),
+                LoopStart(1, 11),    // 4
+                IncPtr(1),
+                LoopStart(2, 11),    // 6
+                DecByte(1),
+                LoopEnd(1, 7),      // 8
+                IncPtr(1),
+                LoopEnd(2, 7),      // 10
+                IncPtr(1),
+                LoopEnd(3, 3),      // 12
+            ],
+        );
+    }
+
+    #[test]
+    fn parse_infinite_loop() {
+        let result = parse("[]");
+        assert_eq!(Err(Error::InfiniteLoop), result);
+    }
+
+    #[test]
+    fn parse_missing_loop_start() {
+        let result = parse("[-]]");
         assert_eq!(Err(Error::UnbalancedBrackets), result);
     }
 
     #[test]
-    fn parse_missing_close_loop() {
-        let result = parse("[[]");
+    fn parse_missing_loop_end() {
+        let result = parse("[[-]");
         assert_eq!(Err(Error::UnbalancedBrackets), result);
     }
 }
