e813792ea750ba5e7d99a0bfaccfee77fa05f78e
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2020-10-24 18:29:02 -0400

    cleans up aarch64 examples

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM ubuntu:20.04
-
-# qemu-system
 RUN apt-get update && apt-get -y install curl clang qemu gcc-aarch64-linux-gnu
-
 RUN curl https://wasmtime.dev/install.sh -sSf | bash
--- examples/aarch64/hello_world.s
+++ examples/aarch64/hello_world.s
@@ -28,25 +28,3 @@ _start:
     mov x8, #SYS_EXIT
     mov x0, #SUCCESS
     svc #0
-
-
-
-
-//.text 			// code section
-//.globl _start
-//_start:
-//	mov x0, 1 	// stdout has file descriptor 1
-//	ldr x1, =msg 	// buffer to write
-//	mov x2, msg_len	// size of buffer
-//	mov x8, 64 	// sys_write() is at index 64 in kernel functions table
-//	svc #0 		// generate kernel call sys_write(stdout, msg, len);
-
-//	mov x0, 123 	// exit code
-//	mov x8, 93 	// sys_exit() is at index 93 in kernel functions table
-//	svc #0 		// generate kernel call sys_exit(123);
-
-//.data			// data section
-//msg: 
-//	.ascii "Hello from Docker."
-
-// msg_len = . - msg 
